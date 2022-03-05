bba29e2d49d949c247bf1b9a85903dacf40c0798
Thomas Laukkanen <66567520+ThomasLaukkanen@users.noreply.github.com>
2022-03-05 17:46:47 +0100

    all

---
+++ packages/api-server/index.ts
@@ -0,0 +1,50 @@
+import express from 'express'
+import * as trpc from '@trpc/server'
+import * as trpcExpress from '@trpc/server/adapters/express'
+import cors from 'cors'
+import { z } from 'zod'
+const app = express()
+const port = 8080
+app.use(cors())
+
+interface ChatMessage {
+  user: string
+  message: string
+}
+
+const messages: ChatMessage[] = [
+  { user: 'user1', message: 'hello' },
+  { user: 'user2', message: 'Hi' }
+]
+
+const appRouter = trpc
+  .router()
+  .query('hello', {
+    resolve() {
+      return 'Hello world2'
+    }
+  })
+  .query('getMessages', {
+    input: z.number().default(10),
+    resolve({ input }) {
+      return messages.slice(-input)
+    }
+  })
+
+export type appRouter = typeof appRouter
+
+app.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext: () => null
+  })
+)
+
+app.get('/', (req, res) => {
+  res.send('Hello from api-server')
+})
+
+app.listen(port, () => {
+  console.log(`api-server listening at http://localhost:${port}`)
+})
---
+++ packages/client/src/App.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import './index.scss'
+import { trpc } from './trpc'
+
+const client = new QueryClient()
+
+const AppContent = () => {
+  const getMessages = trpc.useQuery(['getMessages'])
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <div>{JSON.stringify(getMessages.data)}</div>
+    </div>
+  )
+}
+
+const App = () => {
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      url: 'http://localhost:8080/trpc'
+    })
+  )
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={client}>
+      <QueryClientProvider client={client}>
+        <AppContent />
+      </QueryClientProvider>
+    </trpc.Provider>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
